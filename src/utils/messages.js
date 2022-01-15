4f5f76200bee66f6ca8c193ef1af65bdb3acf226
abcbbxhdhd <ajax76@protonmail.com>
2022-01-16 00:38:29 +0200

    Updated Chat-App

---
+++ src/utils/messages.js
@@ -0,0 +1,19 @@
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username, latitude, longtitude) => {
+    return {
+        username,
+        url: `https://google.com/maps?q=${latitude},${longtitude}`,
+        createdAt: new Date().getTime()
+    }
+}
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
